dbd2679fef2ce97cdac3039897d588e17a196a88
souadmohammed <souadalhashmi--20@hotmail.com>
2021-12-20 14:47:36 +0300

    Add delete and put

--- backend/server.js
+++ backend/server.js
@@ -17,10 +17,33 @@ app.use(express.json())
 //عباره عن فنكشن تتنادى قبل الريكويست 
 //
 
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+/**
+ * post:       insert new item in db (User.create)
+ * 
+ * get:        print all items in db (User.find)
+ * get:        find some name in DB
+ * get:        but some condition in the obj of find func
+ * 
+ * delete:     Deletes the first document that matches conditions (User.deleteOne)
+ * 
+ * put:        update only the first document that matches filter (User.updateOne)
+ */
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+// test the server 
 app.get('/',(req,res)=>{
     res.json('Get / is working ');
 })
 
+
+// get all data from db
 app.get('/tasks',(req,res)=>{
     Todo.find({},(err,data)=>{
         if(err){
@@ -33,6 +56,8 @@ app.get('/tasks',(req,res)=>{
     //res.json('Get / is working ');
 })
 
+
+//add new element in the db get data from req.body of post (must use middleware which is read the body of req)
 app.post('/tasks',(req,res)=>{
     console.log('25:',req.body);
 
@@ -48,6 +73,71 @@ app.post('/tasks',(req,res)=>{
     //res.json('Get / is working ');
 })
 
+
+//delete
+
+/**
+ * Deletes the first document that matches conditions
+ * from the collection. It returns an object with the
+ * property deletedCount indicating how many documents
+ * were deleted.
+ */
+
+// احط بالبارمز اسم اليوزر الي حأعمله حذف
+//يرجع رسبونس واحد 
+app.delete("/tasks/:title", (req, res) => {
+    console.log(req.params);
+    //     الشرط    key : value
+    Todo.deleteOne({ title: req.params.title }, (err, deleteObj) => {
+      if (err) {
+        console.log("ERROR", err);
+        res.status(500).json("there is a problem in DB");
+      } else {
+        // اذا دورت ومالقت اي واحد بدا الاسم
+        // console.log(deleteObj)
+        if (deleteObj.deletedCount === 0) {
+          res.status(404).json("title Not Found");
+        } else {
+          res.status(200).json("Success Delete " + req.params.title);
+        }
+      }
+    });
+  });
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+/**
+ * MongoDB will update only the first document that matches 
+ * filter regardless of the value of the multi option.
+ *    res.matchedCount; // Number of documents matched
+      res.modifiedCount; // Number of documents modified
+      res.upsertedCount; // Number indicating how many documents had to be upserted. Will either be 0 or 1.
+
+ */
+// Extension: Prettier  ||  Shortcut: Alt + Shift + F
+// حطينا / قبل الافنيم عشانها مو متغير والقيمه حتكون في البدي حق الريكويست
+app.put("/tasks/isCompleted/:oldState", (req, res) => {
+    console.log(req.params);
+    Todo.updateOne(
+      { title: req.params.oldState }, // filter «Object» الشرط
+      { isCompleted: req.body.isCompleted }, //update «Object|Array»
+      (err, updateObj) => {
+        //callback func
+        if (err) {
+          console.log("ERROR", err);
+          res.status(500).json("there is a problem in DB");
+        } else {
+          console.log(updateObj);
+          if (updateObj.matchedCount === 0) {
+            res.status(404).json("User Not Found");
+          } else {
+            res.status(200).json("Success Update one state");
+          }
+        }
+      }
+    );
+  });
 app.listen(port,()=>{
     console.log('SERVER IS WORKING');
 })
